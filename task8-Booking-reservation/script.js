502528d31f0d433bb076597187ee4f968d16f3d2
pawan <vishwakarmapawan821@gmail.com>
2024-12-06 13:26:27 +0530

    creating weekly task 9 ai driven

---
+++ task8-Booking-reservation/script.js
@@ -0,0 +1,9 @@
+
+
+
+
+const budgetSlider = document.getElementById('budget');
+        const budgetValue = document.getElementById('budget-value');
+        budgetSlider.addEventListener('input', () => {
+            budgetValue.textContent = `${budgetSlider.value} rupees`;
+        });
